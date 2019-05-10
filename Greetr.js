434a99cb43220b356d46d498fced073d9b961975
BEER-DELL\lgmcb <droidstick@gmail.com>
2019-05-10 12:01:09 +0700

    feat: Initialize Greetr framework object and its prototype

--- Greetr.js
+++ Greetr.js
@@ -1,4 +1,19 @@
 (function(global, $) {
+    var Greetr = function(firstName, lastName, language) {
+        return new Greetr.init(firstName, lastName, language);
+    }
 
+    Greetr.prototype = {};
+
+    Greetr.init = function(firstName, lastName, language){
+        var self = this;
+        self.firstName = firstName || '';
+        self.lastName = lastName || '';
+        self.language = language || 'en';
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.G$ = global.Greetr = Greetr;
 
 }(window, jQuery));
