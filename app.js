214f64c04cf1e740411c47ad21ec23eb31427471
ben <ben@unixdev.co.th>
2019-06-24 18:03:11 +0700

    chore: Try manipulating HTML with Greetr and jQuery

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 var g = G$('ben', 'watee');
 console.log(g);
 g.greet().setLang('es').greet(true).log();
+
+$('#login').click(function() {
+    var loginGreetr = G$('Ben', 'Watee');
+
+    $('#logindiv').hide();
+
+    loginGreetr.setLang($('#lang').val()).HTMLGreeting('#greeting', true).log();
+});
